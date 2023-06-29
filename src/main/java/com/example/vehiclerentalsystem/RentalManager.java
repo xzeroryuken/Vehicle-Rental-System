63d8c32176cbfc5d37504767e02f7ce50ec0ddf1
xzeroryuken <alexander.h.wilson@gmail.com>
2023-06-29 18:01:00 -0400

    Finished the rest of the rent functions to return a numbered list of vehicles for each type, can also choose which vehicle to rent by typing in the number of the vehicle entry.

--- src/main/java/com/example/vehiclerentalsystem/RentalManager.java
+++ src/main/java/com/example/vehiclerentalsystem/RentalManager.java
@@ -57,24 +57,68 @@ public class RentalManager
     private static void rentCar() {
         System.out.println("Which car would you like to rent?: ");
         List<Car> availableCars = Car.getAvailableCars(cars);
-        for (Car car : availableCars) {
-            System.out.println(car.getDetails());
+
+        // Display available cars
+        for (int i = 0; i < availableCars.size(); i++) {
+            System.out.println((i + 1) + ". " + availableCars.get(i).getDetails());
         }
+
+        Scanner scanner = new Scanner(System.in);
+        int selectedCarIndex = scanner.nextInt();
+
+        // Check if the selected index is valid.
+        if (selectedCarIndex >= 1 && selectedCarIndex <= availableCars.size()) {
+            Car selectedCar = availableCars.get(selectedCarIndex - 1);
+            // Perform the rental operation for the selected car
+            selectedCar.isRented();
+            System.out.println("You have successfully rented the car: " + selectedCar.getDetails());
+        } else {
+            System.out.println("Invalid car number. Please try again.");
+        }
+
     }
 
     private static void rentBike() {
         System.out.println("Which bike would you like to rent?: ");
         List<Bicycle> availableBikes = Bicycle.getAvailableBikes(bikes);
-        for (Bicycle bike : availableBikes) {
-            System.out.println(bike.getDetails());
+
+        for (int i = 0; i < availableBikes.size(); i++ ) {
+            System.out.println((i + 1) + ". " + availableBikes.get(i).getDetails());
         }
+
+        Scanner scanner = new Scanner(System.in);
+        int selectedBikeIndex = scanner.nextInt();
+
+        if (selectedBikeIndex >= 1 && selectedBikeIndex <= availableBikes.size()) {
+            Bicycle selectedBike = availableBikes.get(selectedBikeIndex - 1);
+
+            selectedBike.isRented();
+            System.out.println("You have successfully rented the bike");
+        } else {
+            System.out.println("Invalid car number. Please try again");
+        }
+
     }
 
     private static void rentMotorcycle() {
         System.out.println("Which motorcycle would you like to rent?: ");
         List<Motorcycle> availableMotorcycles = Motorcycle.getAvailableMotorcycles(motorcycles);
-        for (Motorcycle motorcycle : availableMotorcycles) {
-            System.out.println(motorcycle.getDetails());
+        for (int i = 0; i < availableMotorcycles.size(); i++) {
+            System.out.println((i + 1) + ". " + availableMotorcycles.get(i).getDetails());
         }
+
+        Scanner scanner = new Scanner(System.in);
+        int selectedMotorcycleIndex = scanner.nextInt();
+
+        if (selectedMotorcycleIndex >= 1 && selectedMotorcycleIndex <= availableMotorcycles.size()) {
+            Motorcycle selectedMotorcycle = availableMotorcycles.get(selectedMotorcycleIndex - 1);
+
+            selectedMotorcycle.isRented();
+            System.out.println("You successfully rented the motorcycle");
+        } else {
+            System.out.println("Invalid Motorcycle number. Please try again");
+
+        }
+
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,9 @@
 module com.example.vehiclerentalsystem {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+    requires org.json;
+
+
     opens com.example.vehiclerentalsystem to javafx.fxml;
     exports com.example.vehiclerentalsystem;
     exports com.example.vehiclerentalsystem.models;
